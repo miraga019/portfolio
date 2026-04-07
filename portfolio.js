beec8baebf1d6600cb30833f385a06b8840ad0de
miraga019 <miraga019@gmail.com>
2026-04-07 13:39:33 +0300

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -108,6 +108,7 @@ const mockups = {
     29: ['./assets/work-29.jpg', './assets/work-29-2.jpg', './assets/work-29-3.jpg'],
     30: ['./assets/work-31.png', './assets/work-31-1.png', './assets/work-31-2.png'],
     31: ['./assets/work-32.jpg', './assets/work-32-2.jpg', './assets/work-32-3.jpg'],
+    31: ['./assets/work-33.jpg', './assets/saffron.mp4', './assets/work-30-2.jpg', './assets/work-30-3.jpg', './assets/work-30-4.jpg'],
 };
 
 items.forEach((item, index) => {
